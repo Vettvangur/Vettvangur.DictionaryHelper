08672049f68fd52fd87176010c2d2448d6a2b378
John <john.sheard@drawgroup.com>
2019-05-22 16:27:25 +0100

    Change to ordinal ignore as we do not want to be invariant on culture

--- Service.cs
+++ Service.cs
@@ -46,9 +46,9 @@ namespace DictionaryHelper
 				key = keys.Last();
 			}
 
-			if (DictionaryCache._cache.Any(x => x.Value.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase) && x.Value.Culture.Equals(culture, StringComparison.InvariantCultureIgnoreCase))) {
+			if (DictionaryCache._cache.Any(x => x.Value.Key.Equals(key, StringComparison.OrdinalIgnoreCase) && x.Value.Culture.Equals(culture, StringComparison.OrdinalIgnoreCase))) {
 
-				var dict = DictionaryCache._cache.FirstOrDefault(x => x.Value.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase) && x.Value.Culture.Equals(culture, StringComparison.InvariantCultureIgnoreCase));
+				var dict = DictionaryCache._cache.FirstOrDefault(x => x.Value.Key.Equals(key, StringComparison.OrdinalIgnoreCase) && x.Value.Culture.Equals(culture, StringComparison.OrdinalIgnoreCase));
 
 				if (!string.IsNullOrEmpty(dict.Value.Value))
 				{
