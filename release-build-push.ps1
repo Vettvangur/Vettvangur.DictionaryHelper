1ee6978e80bc1b86bd1627b9ef2ab6c438e298e4
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-07-22 11:06:06 +0000

    Update u8 dependencies and nuspec

--- release-build-push.ps1
+++ release-build-push.ps1
@@ -1,6 +1,9 @@
 nuget restore .
+Remove-Item *.nupkg
+Remove-Item *.snupkg
 nuget pack -build -Symbols -SymbolPackageFormat snupkg -Properties Configuration=Release
 $pkg = gci *.nupkg 
-nuget push $pkg -Source https://www.nuget.org/api/v2/package -NonInteractive
+nuget push $pkg -Source https://www.nuget.org/api/v2/package -NonInteractive -SkipDuplicate
 $snupkg = gci *.snupkg 
-nuget push $snupkg -Source https://www.nuget.org/api/v2/package -NonInteractive
+Write-Host Pushing symbols to https://www.nuget.org/api/v2/package
+nuget push $snupkg -Source https://www.nuget.org/api/v2/package -NonInteractive -verb
