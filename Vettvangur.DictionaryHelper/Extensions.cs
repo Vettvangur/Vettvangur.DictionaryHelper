15487bc1fee43e1e542edf234c85ef2d89428457
Garðar Þorsteinsson <gardar@vettvangur.is>
2023-01-13 16:34:58 +0000

    v7

--- Vettvangur.DictionaryHelper/Extensions.cs
+++ Vettvangur.DictionaryHelper/Extensions.cs
@@ -3,7 +3,7 @@ using Umbraco.Cms.Web.Common;
 
 namespace DictionaryHelper
 {
-    public class Extensions
+    public static class Extensions
 	{
 		/// <summary>
 		/// Returns the dictionary value for the key specified
@@ -13,7 +13,7 @@ namespace DictionaryHelper
 		/// <param name="parentKey">Optional: Key will be created as a child of the parent key.</param>
 		/// <param name="create">Optional: If key does not exist it will be created. Requires default value.</param>
 		/// <returns>Dictionary string value</returns>
-		public string DictionaryValue(UmbracoHelper helper, string key, string defaultValue = null, string parentKey = null, bool create = false)
+		public static string DictionaryValue(UmbracoHelper helper, string key, string defaultValue = null, string parentKey = null, bool create = false)
 		{
 			//var content = helper.AssignedContentItem;
 
